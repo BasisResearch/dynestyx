746d926807963fcfac15113366dac7716db04921
Dan Waxman <dan.waxman1@gmail.com>
2026-02-18 11:29:39 -0500

    Uncomment test

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -3,20 +3,22 @@ import jax.random as jr
 from numpyro.handlers import seed, trace
 
 from tests.fixtures import (
+    data_conditioned_jumpy_controls,
     data_conditioned_jumpy_controls_sde,
 )
 
-# def test_jumpy_controls():
-#     data_conditioned_model, synthetic = data_conditioned_jumpy_controls()
-#     rng_key = jr.PRNGKey(0)
-#     with trace() as tr, seed(rng_seed=rng_key):
-#         data_conditioned_model()
 
-#     synthetic_observations = synthetic["observations"][0, ...]
-#     filtered_means = tr["f_filtered_states_mean"]["value"]
-#     assert synthetic_observations.shape == filtered_means.shape
-#     assert jnp.allclose(synthetic_observations, filtered_means, atol=1e0)
-#     assert jnp.abs(jnp.mean(synthetic_observations - filtered_means)) < 1e-2
+def test_jumpy_controls():
+    data_conditioned_model, synthetic = data_conditioned_jumpy_controls()
+    rng_key = jr.PRNGKey(0)
+    with trace() as tr, seed(rng_seed=rng_key):
+        data_conditioned_model()
+
+    synthetic_observations = synthetic["observations"][0, ...]
+    filtered_means = tr["f_filtered_states_mean"]["value"]
+    assert synthetic_observations.shape == filtered_means.shape
+    assert jnp.allclose(synthetic_observations, filtered_means, atol=1e0)
+    assert jnp.abs(jnp.mean(synthetic_observations - filtered_means)) < 1e-2
 
 
 def test_jumpy_controls_sde():
@@ -27,7 +29,6 @@ def test_jumpy_controls_sde():
 
     synthetic_observations = synthetic["observations"][0, ...]
     filtered_means = tr["f_filtered_states_mean"]["value"]
-    assert False
     assert synthetic_observations.shape == filtered_means.shape
     assert jnp.allclose(synthetic_observations, filtered_means, atol=1e0)
     assert jnp.abs(jnp.mean(synthetic_observations - filtered_means)) < 1e-2
