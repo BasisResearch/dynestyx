e777e405d1899bb335355a01a1108e2f8afbe7f9
Matthew Levine <mattlevine22@gmail.com>
2026-02-20 09:55:38 -0500

    removing unused classes Trajectory and Context

--- dynestyx/dynamical_models.py
+++ dynestyx/dynamical_models.py
@@ -204,36 +204,3 @@ class GaussianStateEvolution(DiscreteTimeStateEvolution):
         loc = self.F(x, u, t_now, t_next)
 
         return dist.MultivariateNormal(loc=loc, covariance_matrix=self.cov)
-
-
-@dataclasses.dataclass
-class Trajectory:
-    """
-    A 1D time axis and values living on that axis.
-
-    Semantics:
-      - times is None  -> times are implicit / inferred / shared with some other grid
-      - values is None -> "no values here" (e.g. just a solve grid)
-    """
-
-    times: Time | None = None
-    values: State | None = None
-
-
-@dataclasses.dataclass
-class Context:
-    """
-    All time-indexed info for a single sample site.
-    """
-
-    # Where to solve the dynamics
-    solve: Trajectory = dataclasses.field(default_factory=Trajectory)
-
-    # Observations
-    observations: Trajectory = dataclasses.field(default_factory=Trajectory)
-
-    # Controls u(t), if any
-    controls: Trajectory = dataclasses.field(default_factory=Trajectory)
-
-    # Extensible: extra time-indexed series or metadata
-    extras: dict[str, Trajectory] = dataclasses.field(default_factory=dict)
