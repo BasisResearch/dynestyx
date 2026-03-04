9607c5774b50a3f02c2524b4956d5fbd0509e722
Dan Waxman <dan.waxman1@gmail.com>
2026-03-04 16:12:21 -0500

    Update documentation for new sample structure
    This adds docstrings for sample, and updates the content of _sample_intp docstrings.

--- dynestyx/handlers.py
+++ dynestyx/handlers.py
@@ -26,8 +26,33 @@ def sample(
     ctrl_values: jax.Array | None = None,
     **kwargs,
 ) -> FunctionOfTime:
+    """
+    Samples from a dynamical model. This is the main primitive of dynestyx.
+
+    The `sample` primitive is meant to mimic the `numpyro.sample` primitive in usage,
+    but using a `DynamicalModel` instead of a `Distribution`.
+
+    The `sample` method calls `_sample_intp`, which is defined as a `defop` in `effectful`.
+    This is where any real "work" is done, after input validation.
+
+    Parameters:
+        name: Name of the sample site.
+        dynamics: Dynamical model to sample from.
+        obs_times: Times at which to sample the observations.
+        obs_values: Values of the observations at the given times.
+        ctrl_times: Times at which to sample the controls.
+        ctrl_values: Values of the controls at the given times.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        FunctionOfTime: A function of time that samples from the dynamical model.
+    """
     _validate_site_sorting(obs_times, ctrl_times)
+
+    # Initial dynamics may not have t0, which is then inferred from obs_times
     dynamics_with_t0 = _get_dynamics_with_t0(dynamics, obs_times)
+
+    # Pass to interpreted version of `sample` for inference.
     return _sample_intp(
         name,
         dynamics_with_t0,
@@ -51,13 +76,10 @@ def _sample_intp(
     **kwargs,
 ) -> FunctionOfTime:
     """
-    Samples from a dynamical model. This is the main primitive of dynestyx.
+    The functional version of `sample` to be interpreted at runtime.
 
-    The `sample` primitive is meant to mimic the `numpyro.sample` primitive in usage,
-    but using a `DynamicalModel` instead of a `Distribution`.
-
-    The `sample` method is defined as a `defop` in `effectful`, meaning that
-    it is an undefined function here, but "interpreted" at runtime. In other words,
+    This is implemented as a `defop` in `effectful`, meaning it is
+    an undefined function here, but "interpreted" at runtime. In other words,
     the actual implementation of a `sample` operation is determined by the context
     in which it is used, e.g., within a `Filter` or `Simulator` object.
 
