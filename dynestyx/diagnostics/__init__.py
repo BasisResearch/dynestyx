2d88149a62b3d4e3d94a14d8972c182e2415fce6
Dan Waxman <dan.waxman1@gmail.com>
2026-08-17 13:31:21 -0400

    Add deprecration notice for diagnostics/plotting_utils

---
+++ dynestyx/diagnostics/__init__.py
@@ -0,0 +1,5 @@
+"""Deprecated diagnostics namespace.
+
+Use :mod:`dynestyx.evaluation` instead. Compatibility imports under
+``dynestyx.diagnostics`` are deprecated and will be removed in v0.4.0.
+"""
